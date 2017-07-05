11ed79775784e4d3a6d5b2ab7cdcc8a1c71633e0
hzbhzb <1012734765@qq.com>
2017-07-05 13:49:44 +0800

    添加第三方应用管理界面

---
+++ app/src/main/java/appmanager/com/appmanager/AppManagerActivity.java
@@ -0,0 +1,13 @@
+package appmanager.com.appmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_manager);
+    }
+}
